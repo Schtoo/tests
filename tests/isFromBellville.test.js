fe86a5d073dafefb92bcc8b42864bb9420934cce
Schtoo <mgqobhoz@gmail.com>
2018-04-09 14:27:56 +0200

    added isFromBellville test

--- tests/isFromBellville.test.js
+++ tests/isFromBellville.test.js
@@ -1,8 +1,8 @@
 describe('isFromBellville', function(){
     it('should give you a Bellville number plate', function(){
-        assert.equal(isFromBellville ('CY 564-987, CF 432-656, L 245'), true)
+        assert.equal(isFromBellville ('CY 564-987'), true)
     });
     it('should give you a false message if it is not fromBellville', function(){
-      assert.equal(isFromBellville('CA 898-876, 2314-GP, MP 2233'), false)
+      assert.equal(isFromBellville('CA 898-876'), false)
     });
 });
