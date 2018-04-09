19a593383c429275b295116bda5fff6ab53a2c4e
Schtoo <mgqobhoz@gmail.com>
2018-04-09 14:21:27 +0200

    added regCheck changes

--- tests/regCheck.test.js
+++ tests/regCheck.test.js
@@ -1,9 +1,14 @@
 describe('regCheck', function(){
-    it('should show you Gauteng, Mpumalanga, Limpopo & Eastern Cape number plates and reg numbers', function(){
-        assert.equal(true,  regCheck('DV 23 NB GP', 'GP', 'DV 23 LP MP', 'MP', '189-012 L', 'L', '189-012 EC', 'EC'))
-
+    it('should show you Gauteng number plate and reg number', function(){
+        assert.deepEqual(regCheck('DV 23 NB GP','GP',), true)
     });
-    it('should show you Gauteng, Mpumalanga, Limpopo & Eastern Cape number plates and reg numbers', function(){
-      assert.equal(false,  regCheck('DV 23 NB GP', 'CA', 'DV 23 LP MP', 'CY', '189-012 L', 'CZ', '189-012 EC', 'CJ'))
+    it('should give you Mpumalanga registration number', function(){
+      assert.deepEqual(regCheck('DV 23 LP MP', 'MP'), true)
+    });
+    it('should give you Limpopo registration number', function(){
+      assert.deepEqual(regCheck('189-012 L', 'L'), true)
+    });
+    it('should show you a reg number from Eastern Cape', function(){
+      assert.deepEqual(regCheck('189-012 EC', 'EC'), true)
     });
 });
