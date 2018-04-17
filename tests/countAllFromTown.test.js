6467b508f74fa364c5a3a29c414a9c3cc1166445
Schtoo <mgqobhoz@gmail.com>
2018-04-17 09:28:32 +0200

    added mostProfitable

--- tests/countAllFromTown.test.js
+++ tests/countAllFromTown.test.js
@@ -1,6 +1,6 @@
 describe('countAllFromTown', function(){
   it('should give you a number of reg numbers from Cape Town', function(){
-    assert.deepEqual(countAllFromTown ('CY 567, CL 345, CJ 456, CL 341','CL'), 2)
+    assert.deepEqual(countAllFromTown ('CY 567, CL 345, CJ 456, CL 341','CL'), 3)
   });
   it('should show you a false message if reg numbers are not from Cape Town', function(){
     assert.notEqual(countAllFromTown('234 55 L,CX 135 435,1234 MP', 'GP'), true)
--- tests/findItemsOver20.test.js
+++ tests/findItemsOver20.test.js
@@ -1,14 +1,16 @@
+
 describe('findItemsOver20', function(){
   it('should give you the amount of objects more than 20', function(){
-    assert.deepEqual([], findItemsOver20 (20), 2);
+    assert.deepEqual([
+        {name : 'pears', qty : 37},
+        {name : 'bananas', qty : 27}], findItemsOver20([
+        {name : 'apples', qty : 10},
+        {name : 'pears', qty : 37},
+        {name : 'bananas', qty : 27},
+        {name : 'apples', qty : 3},
+    ]));
   });
   it('should give you the value 2 and not anything higher or lower', function(){
-    assert.notDeepEqual(2, findItemsOver20(20), 3)
-  });
-  it('should give show you a message stating an amount less than 2 is below the expected amount', function(){
-    assert.isBelow(1, 2, '1 is below 2')
-  });
-  it('should give you a message showing you have entered a higher amount than expected', function(){
-    assert.isAtMost(2, 4, '4 is higher or equal to 2')
+    assert.notDeepEqual('', findItemsOver20(fruits))
   });
 });
